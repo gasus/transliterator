d38de3f5a4371ec1a992dedce2b3fd77f837dfc3
Gasu <zgas@mail.ru>
2023-12-23 02:05:28 +0500

    add adaptive demo

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ let MODE = "jp";
 const select = document.querySelector("#select");
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
+const copyBtn = document.querySelector("#copy");
 
 const changeOutput = () => {
   output.value = transliteration({ text: input.value, mode: MODE });
+  copyBtn.disabled = !output.value.length;
 };
 
 select.addEventListener("change", () => {
@@ -19,6 +21,11 @@ input.addEventListener("input", () => {
   changeOutput();
 });
 
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(output.value);
+});
+
 window.onload = () => {
   select.value = MODE;
+  copyBtn.disabled = true;
 };
